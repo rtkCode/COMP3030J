e0c9036c681257bd3c22b7ed7f848a51099ae36a
Tianze Wen <tianze.wen@ucdconnect.ie>
2020-03-08 19:23:19 +0800

    update front-end login

--- front-end/src/views/LogIn.vue
+++ front-end/src/views/LogIn.vue
@@ -1,225 +1,116 @@
 <template>
-
   <div>
-
     <Header :hospital="hospital" ref="header"></Header>
-
-    <section class="content d-flex justify-content-center align-items-center">
-
+    <section class="content d-flex flex-column justify-content-center align-items-center">
       <form class="needs-validation" novalidate>
-
         <div class="form">
 
-          <div class="row mt-1">
-
+          <div class="row">
             <label for="validationCustomUsername">Username</label>
-
             <div class="input-group">
-
               <div class="input-group-prepend">
-
                 <span class="input-group-text" id="inputGroupPrepend">@</span>
-
               </div>
-
-              <input
-
-                type="text"
-
-                class="form-control"
-
-                id="validationCustomUsername"
-
-                aria-describedby="inputGroupPrepend"
-
-                required
-
-              />
-
-              <div class="invalid-feedback">Please enter your username.</div>
-
+              <input type="text" v-model="username" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required />
             </div>
-
+            <small class="invalid">*Username format invalid</small>
           </div>
 
           <div class="row mt-1">
-
             <label for="pw">Password</label>
-
-
-
-            <input type="password" class="form-control" id="pw" required />
-
-
-
-            <div class="invalid-feedback">Please enter your password.</div>
-
+            <input type="password" v-model="password" class="form-control" id="pw" required />
+            <small class="invalid">*Password format invalid</small>
           </div>
-
         </div>
-
-
-
-        <br />
-
-
-
-        <button class="btn btn-outline-info col-6" type="submit">Log In</button>
-
       </form>
-
+      <button class="btn btn-outline-info mt-3 px-4" type="submit" @click="verifyUsername()">Log in</button>
     </section>
-
-
-
     <Footer :hospital="hospital"></Footer>
-
   </div>
-
 </template>
 
 
 
 <script>
-
 import Header from "@/components/Header.vue";
-
-
-
 import SideBar from "@/components/SideBar.vue";
-
-
-
 import Footer from "@/components/Footer.vue";
 
-
-
 export default {
+  data(){
+    return{
+      loginUrl: "https://jsonplaceholder.typicode.com/posts",
+      username: "",
+      password: ""
+    }
+  },
 
   props: {
-
     hospital: String
-
   },
-
-
 
   components: {
-
     Header,
-
-
-
     SideBar,
-
-
-
     Footer
-
   },
-
-
 
   mounted() {
-
+    $(".invalid").hide();
     this.hilight();
-
-
-
-    this.verifyText();
-
   },
-
-
 
   created() {
-
     document.title = `Log In | ${this.hospital}`;
-
   },
 
-
-
   methods: {
-
     hilight() {
-
       let dom = this.$refs.header.$refs.login;
-
-
-
       $(dom).addClass("active");
-
     },
 
+    verifyUsername(){
+      let usernameReg=/^[a-zA-Z]{1}([a-zA-Z0-9]|[_]){3,15}$/;
+      if(usernameReg.test(this.username)){
+        $(".invalid").eq(0).hide();
+        this.verifyPassword();
+      }else{
+        $(".invalid").eq(0).show();
+      }
+    },
 
+    verifyPassword(){
+      let passwordReg=/^(\S){6,18}$/;
+      if(passwordReg.test(this.password)){
+        $(".invalid").eq(1).hide();
+        this.login();
+      }else{
+        $(".invalid").eq(1).show();
+      }
+    },
 
-    verifyText() {
-
-      "use strict";
-
-
-
-      window.addEventListener(
-
-        "load",
-
-
-
-        function() {
-
-          let forms = document.getElementsByClassName("needs-validation");
-
-
-
-          let validation = Array.prototype.filter.call(forms, function(form) {
-
-            form.addEventListener(
-
-              "submit",
-
-
-
-              function(event) {
-
-                if (form.checkValidity() === false) {
-
-                  event.preventDefault();
-
-
-
-                  event.stopPropagation();
-
-                }
-
-
-
-                form.classList.add("was-validated");
-
-              },
-
-
-
-              false
-
-            );
-
-          });
-
-        },
-
-
-
-        false
-
-      );
-
-    }
+    login(){
+      console.log("ok");
+      this.$axios({
+        method: 'post',
+        url: this.loginUrl,
+        data: {
+          username: this.username,
+          password: this.password
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    },
 
   }
 
 };
-
 </script>
 
 
@@ -227,11 +118,10 @@ export default {
 
 
 <style scoped>
-
 .content {
-
   height: 90vh;
-
 }
-
+.invalid{
+  color: #FF4136;
+}
 </style> 

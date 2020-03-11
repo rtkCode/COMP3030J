70b102a384f20fb14ea6eb7ff4f99eca20b8a650
Tianze Wen <tianze.wen@ucdconnect.ie>
2020-03-11 16:16:53 +0800

    update front-end sign up hint

--- front-end/src/views/SignUp.vue
+++ front-end/src/views/SignUp.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <Header :hospital="hospital" ref="header"></Header>
+    <div id="toast-container" aria-live="polite" aria-atomic="true">
+      <div class="toast border rounded-lg" role="alert" aria-live="assertive" aria-atomic="true" data-delay="15000" style="right: 10; top: 70;">
+        <div class="toast-header">
+          <strong class="mr-auto">{{registerHintTitle}}</strong>
+          <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="toast-body text-white">{{registerHintText}}</div>
+      </div>
+    </div>
     <section class="content d-flex flex-column justify-content-center align-items-center">
       <form class="needs-validation" novalidate>
         <div class="form">
@@ -22,15 +33,17 @@
               <div class="input-group-prepend">
                 <span class="input-group-text" id="inputGroupPrepend">@</span>
               </div>
-              <input type="text" class="form-control" v-model="username" @blur="verifyUserId()" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
+              <input type="text" class="form-control" v-model="username" @blur="verifyUserId('username')" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
             </div>
             <small class="invalid">*4-16 digits, letters, numbers, underscore</small>
+            <small class="error-text">{{usernameErrorText}}</small>
           </div>
 
           <div class="row mt-1">
             <label for="ea">Email address</label>
-            <input type="email" class="form-control" id="ea" v-model="email" @blur="verifyUserId()" required>
+            <input type="email" class="form-control" id="ea" v-model="email" @blur="verifyUserId('email')" required>
             <small class="invalid">*Please input the correct email address</small>
+            <small class="error-text">{{emailErrorText}}</small>
           </div>
 
           <div class="row mt-1">
@@ -41,9 +54,11 @@
         </div>
         <small>*By clicking sign up, you agree to the terms and conditions</small>
       </form>
-
-      <button class="btn btn-outline-info mt-2 px-4" type="submit" @click="verifyName()">Sign up</button>
-
+      <button class="btn btn-outline-info mt-2 px-4" type="submit" @click="verifyName()" v-show="showButton">Sign up</button>
+      <button class="btn btn-outline-info mt-2 px-4" type="button" v-show="!showButton" disabled>
+        <span class="spinner-border spinner-border-sm mb-1" role="status" aria-hidden="true"></span>
+        Loading...
+      </button>
     </section>
     <Footer :hospital="hospital"></Footer>
   </div>
@@ -59,12 +74,17 @@ export default {
   data(){
     return{
       verifyUserIdUrl: "http://127.0.0.1:5000/verifyUserId",
-      registerUrl: "https://jsonplaceholder.typicode.com/posts",
+      registerUrl: "http://127.0.0.1:5000/register",
       firstName: "",
       lastName: "",
       username: "",
       email: "",
-      password: ""
+      password: "",
+      usernameErrorText: "",
+      emailErrorText: "",
+      registerHintTitle: "",
+      registerHintText: "",
+      showButton: true,
     }
   },
 
@@ -139,44 +159,84 @@ export default {
     },
 
     register(){
+      let _this=this;
+      this.showButton=false;
+
       this.$axios({
         method: 'post',
         url: this.registerUrl,
-        data: {
+        headers:{'Content-Type':'application/x-www-form-urlencoded'},
+        data: this.$qs.stringify({
           firstName: this.firstName,
           lastName: this.lastName,
           username: this.username,
           email: this.email,
           password: this.password,
           others: "test",
-        }
+        })
       })
       .then(function (response) {
-        console.log(response);
+        _this.showButton=true;
+        $('.toast').toast('show');
+        if(response.data.code==200){
+          $(".toast").removeClass("bg-danger border-danger");
+          $(".toast").addClass("bg-success border-success");
+          _this.registerHintTitle="Register success";
+          _this.registerHintText=response.data.msg+", it will jump to the login page in 3 seconds";
+          setTimeout(_this.routeToLogin,3000);
+        }
+        if(response.data.code==400){
+          $(".toast").removeClass("bg-success border-success");
+          $(".toast").addClass("bg-danger border-danger");
+          _this.registerHintTitle="Register failed";
+          _this.registerHintText=response.data.msg+", please correct and resubmit";
+        }
       })
       .catch(function (error) {
         console.log(error);
+        _this.showButton=true;
+        $('.toast').toast('show');
+        $(".toast").removeClass("bg-success border-success");
+        $(".toast").addClass("bg-danger border-danger");
+        _this.registerHintTitle="Unknown error";
+        _this.registerHintText="unknown error, please check console log";
       });
     },
 
-    verifyUserId(){
+    verifyUserId(mode){
+      let data;
+      let _this=this;
+
+      if(mode=="username"){
+        data=this.$qs.stringify({
+          username: this.username
+        });
+      }else if(mode=="email"){
+        data=this.$qs.stringify({
+          email: this.email
+        })
+      }
+
       this.$axios({
         method: 'post',
         url: this.verifyUserIdUrl,
         headers:{'Content-Type':'application/x-www-form-urlencoded'},
-        data: this.$qs.stringify({
-          username: this.username,
-          email: this.email,
-        })
+        data: data,
       })
       .then(function (response) {
-        console.log(response);
+        if(response.data.code==201) _this.usernameErrorText="";
+        if(response.data.code==202) _this.emailErrorText="";
+        if(response.data.code==401) _this.usernameErrorText=response.data.msg;
+        if(response.data.code==402) _this.emailErrorText=response.data.msg;
       })
       .catch(function (error) {
         console.log(error);
       });
     },
 
+    routeToLogin(){
+      this.$router.push({path: '/login',query:{ username: this.username}});
+    }
   }
 }
 </script>
@@ -185,7 +245,24 @@ export default {
 .content{
   height: 90vh;
 }
+
 .invalid{
   color: #FF4136;
+}
+
+.error-text{
+  color: #FF4136;
+}
+
+#toast-container{
+  position: relative;
+}
+
+.toast{
+  position: absolute; 
+  top: 10px;
+  right: 10px;
+  min-width: 300px;
+  /* background: #FF4136; */
 }
 </style>

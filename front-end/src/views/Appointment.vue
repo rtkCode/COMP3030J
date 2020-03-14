869865079dfbe85eb3c3c6d6e13003ece691fca7
Tianze Wen <tianze.wen@ucdconnect.ie>
2020-03-14 13:05:17 +0800

    update front-end appointment page

--- front-end/src/views/Appointment.vue
+++ front-end/src/views/Appointment.vue
@@ -1,47 +1,36 @@
 <template>
   <div class="Appointment">
     <Header :hospital="hospital" ref="header"></Header>
-    <div>
-      <br />
-      <h3>Appointment Date</h3>
-      <ul>
-        <li
-          v-for="date,index of date"
-          :key="item"
-          :class="{checked:index==d}"
-          @click="changeDate(index)"
-        >{{date}}</li>
-      </ul>
-      <hr />
-      <h3>Pet Types</h3>
-      <ul>
-        <li
-          v-for="pet,index of pet"
-          :key="item"
-          :class="{checked:index==p}"
-          @click="changePet(index)"
-        >{{pet}}</li>
-      </ul>
-      <hr />
-      <h3>Location</h3>
-      <ul>
-        <li
-          v-for="city,index of city"
-          :key="item"
-          :class="{checked:index==c}"
-          @click="changeCity(index)"
-        >{{city}}</li>
-      </ul>
-      <hr />
-      <h3>Where do you think your pet is going wrong</h3>
-      <ul>
-        <li
-          v-for="type,index of type"
-          :key="item"
-          :class="{checked:index==t}"
-          @click="changeType(index)"
-        >{{type}}</li>
-      </ul>
+    <div class="d-flex align-items-start flex-column" style="height: 90vh">
+
+      <div class="p-4">
+        <h4 class="text-left">Appointment Date</h4>
+        <div class="d-flex flex-wrap">
+          <button class="btn btn-outline-info button-c p-2 m-2" v-for="(date,index) in dates" :key="index" :class="{checked:index==d}" @click="changeDate(index)">{{date.date}}, {{date.day}}</button>
+        </div>
+      </div>
+
+      <div class="p-4">
+        <h4 class="text-left">Pet Types</h4>
+        <div>
+          <button class="btn btn-outline-info button-c p-2 m-2" v-for="(pet,index) in pets" :key="index" :class="{checked:index==p}" @click="changePet(index)">{{pet}}</button>
+        </div>
+      </div>
+      
+      <div class="p-4">
+        <h4 class="text-left">Location</h4>
+        <div class="d-flex flex-wrap">
+          <button class="btn btn-outline-info button-c p-2 m-2" v-for="(city,index) in cities" :key="index" :class="{checked:index==c}" @click="changeCity(index)">{{city}}</button>
+        </div>
+      </div>
+
+      <div class="p-4">
+        <h4 class="text-left">Where do you think your pet is going wrong</h4>
+        <div class="d-flex flex-wrap">
+          <button class="btn btn-outline-info button-c p-2 m-2" v-for="(type,index) in types" :key="index" :class="{checked:index==t}" @click="changeType(index)">{{type}}</button>
+        </div>
+      </div>
+
     </div>
     <Footer :hospital="hospital"></Footer>
   </div>
@@ -56,143 +45,102 @@ import Footer from "@/components/Footer.vue";
 export default {
   data() {
     return {
-      nowDate: "",
-      nowWeek: "",
-      date: [],
-      pet: ["Dog", "Cat"],
-      city: ["Beijing", "Shanghai", "Chengdu"],
-      type: ["Surgery", "Internal medicine", "Ophthalmology"],
+      dates: [],
+      pets: ["Dog", "Cat"],
+      cities: ["Beijing", "Shanghai", "Chengdu"],
+      types: ["Surgery", "Internal medicine", "Ophthalmology", "Orthopedics", "Dermatology", "Obstetrics"],
+      date: "",
+      petType: "Dog",
+      location: "Beijing",
+      symptom: "Surgery",
+      message: "",
       d: 0,
       p: 0,
       c: 0,
       t: 0
     };
   },
+
   components: {
     Header,
     SideBar,
     Footer
   },
+
   props: {
     hospital: String
   },
+
   created() {
     document.title = this.hospital;
   },
+
   mounted() {
     this.hilight();
-    this.currentTime();
-    this.date.push(this.getBeforeDate(0, this.currentTime()) + this.nowWeek);
-    this.date.push(this.getBeforeDate(-1, this.currentTime()) + this.nowWeek);
-    this.date.push(this.getBeforeDate(-2, this.currentTime()) + this.nowWeek);
-    this.date.push(this.getBeforeDate(-3, this.currentTime()) + this.nowWeek);
-    this.date.push(this.getBeforeDate(-4, this.currentTime()) + this.nowWeek);
-    this.date.push(this.getBeforeDate(-5, this.currentTime()) + this.nowWeek);
-    this.date.push(this.getBeforeDate(-6, this.currentTime()) + this.nowWeek);
+    for(let i=0;i<7;i++){
+      let dict={}
+      dict["date"]=this.getDate(i);
+      dict["day"]=this.getWeekDay(i);
+      this.dates.push(dict);
+    }
+    this.date=this.getDate(0);
   },
+
   created() {
     document.title = `Appointment | ${this.hospital}`;
   },
+
   methods: {
     hilight() {
       let dom = this.$refs.header.$refs.appointment;
       $(dom).addClass("active");
     },
-    currentTime() {
-      setInterval(this.getDate, 500);
+
+    getDate(d){
+      let today=new Date();
+      let day=new Date();
+      day.setTime(today.getTime()+3600*1000*24*d);
+      return day.getFullYear()+"-"+(day.getMonth()+1)+"-"+day.getDate();
     },
-    getBeforeDate(num, time) {
-      let n = num;
-      let d = "";
-      if (time) {
-        d = new Date(time);
-      } else {
-        d = new Date();
-      }
-      let year = d.getFullYear();
-      let mon = d.getMonth() + 1;
-      let day = d.getDate();
-      let week = d.getDay();
-      if (day <= n) {
-        if (mon > 1) {
-          mon = mon - 1;
-        } else {
-          year = year - 1;
-          mon = 12;
-        }
-      }
-      d.setDate(d.getDate() - n);
-      year = d.getFullYear();
-      mon = d.getMonth() + 1;
-      day = d.getDate();
-      week = d.getDay();
-      if (week == 1) {
-        this.nowWeek = "Mon";
-      } else if (week == 2) {
-        this.nowWeek = "Tue";
-      } else if (week == 3) {
-        this.nowWeek = "Wed";
-      } else if (week == 4) {
-        this.nowWeek = "Thu";
-      } else if (week == 5) {
-        this.nowWeek = "Fri";
-      } else if (week == 6) {
-        this.nowWeek = "Sat";
-      } else {
-        this.nowWeek = "Sun";
-      }
-      let date =
-        year +
-        "-" +
-        (mon < 10 ? "0" + mon : mon) +
-        "-" +
-        (day < 10 ? "0" + day : day) +
-        " ";
-      return date;
+
+    getWeekDay(d){
+      let today=new Date();
+      let day=new Date();
+      day.setTime(today.getTime()+3600*1000*24*d);
+      let weekDay="Sun,Mon,Tue,Wed,Thu,Fri,Sat";
+      return weekDay.split(",")[day.getDay()];
     },
+
     changePet(index) {
       this.p = index;
+      this.petType=this.pets[index];
     },
+
     changeCity(index) {
       this.c = index;
+      this.location=this.cities[index];
     },
+
     changeDate(index) {
       this.d = index;
+      this.date=this.dates[index]["date"];
     },
+
     changeType(index) {
       this.t = index;
+      this.symptom=this.types[index];
     }
   }
 };
 </script>
 
 <style scoped>
-.content  {
-  height: 90vh;
-}
-.seven24  {
-  font-size: 70px;
-}
-li {
+
+.button-c{
   width: 150px;
-  height: 50px;
-  display: inline-block;
-  border-radius: 30px;
-  border: 1px #3ba2bd solid;
-  text-align: center;
-  line-height: 50px;
-  cursor: pointer;
-  margin-left: 8px;
-  margin-bottom: 10px;
-  color: #3ba2bd;
-}
-li:hover {
-  background-color: #3ba2bd;
-  color: #fff;
-  border: 1px#fff solid;
 }
 
-li.checked {
+.checked {
   background-color: #3ba2bd;
   color: #fff;
   border: 1px #fff solid;

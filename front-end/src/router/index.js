41fbe25ffce46af4a2de59ffac5fd7c202430a58
Cui21 <735068460@qq.com>
2020-03-19 19:14:26 +0800

    log out page

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -4,6 +4,7 @@ import Index from '../views/Index.vue'
 import LogIn from '../views/LogIn.vue'
 import SignUp from '../views/SignUp.vue'
 import Appointment from '../views/Appointment.vue'
+import LogOut from '../views/LogOut.vue'
 import axios from 'axios'
 import qs from 'qs';
 
@@ -45,6 +46,11 @@ const routes = [
     meta: {
       requireAuth: true
     },
+  },
+  {
+    path: '/logout',
+    name: 'LogOut',
+    component: LogOut
   }
 ]
 
@@ -55,66 +61,66 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  if(to.meta.requireAuth){
-    let token=localStorage.getItem('t');
-    let t=window.decodeURIComponent(window.atob(token));
+  if (to.meta.requireAuth) {
+    let token = localStorage.getItem('t');
+    let t = window.decodeURIComponent(window.atob(token));
 
-    if(token==null){
+    if (token == null) {
       next({
         name: 'LogIn',
-        query:{ 
+        query: {
           message: "In order to complete this operation, you must log in",
           from: to.path
         }
       });
-    }else{
+    } else {
       axios({
         method: 'post',
         url: "http://127.0.0.1:5000/verifyToken",
-        headers:{
+        headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
-          "Authorization": "bearer "+t
+          "Authorization": "bearer " + t
         },
         data: qs.stringify({
           token: token
         })
       })
 
-      .then(function (response) {
-        if(response.data.code==200){
-          next();
-        }else{
-          next("/");
-        }
-      })
+        .then(function (response) {
+          if (response.data.code == 200) {
+            next();
+          } else {
+            next("/");
+          }
+        })
 
-      .catch(function (error) {
-        if(error.response){
-          if(error.response.status==401){
-            localStorage.removeItem('t');
-            next({
-              name: 'LogIn',
-              query:{ 
-                message: "Login status expired, please log in again",
-                from: to.path
-              }
-            });
-          }else{
+        .catch(function (error) {
+          if (error.response) {
+            if (error.response.status == 401) {
+              localStorage.removeItem('t');
+              next({
+                name: 'LogIn',
+                query: {
+                  message: "Login status expired, please log in again",
+                  from: to.path
+                }
+              });
+            } else {
+              console.log(error);
+            }
+          } else {
             console.log(error);
           }
-        }else{
-          console.log(error);
-        }
-      });
+        });
     }
-  }else if(to.meta.toLogout){
-    let token=localStorage.getItem('t');
-    if(token==null){
+  } else if (to.meta.toLogout) {
+    let token = localStorage.getItem('t');
+    if (token == null) {
       next();
-    }else{
+    } else {
       next("/logout")
     }
-  }else{
+  } else {
     next();
   }
 })
---
+++ front-end/src/views/LogOut.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <Header :hospital="hospital" ref="header"></Header>
+
+    <div class="confirm">
+      <h2>Confirm</h2>
+      <hr />
+      <h4>You have already logged in, you need to log out before logging in as different user.</h4>
+      <hr />
+      <button
+        class="btn btn-outline-info rounded-pill mt-2 px-4"
+        @click="logout"
+        id="logout"
+      >log out</button>
+      <button class="btn btn-outline-info rounded-pill mt-2 px-4" @click="cancel" id="cancel">cancel</button>
+    </div>
+
+    <Footer :hospital="hospital"></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import SideBar from "@/components/SideBar.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    SideBar,
+    Footer
+  },
+  props: {
+    hospital: String
+  },
+  created() {
+    document.title = this.hospital;
+  },
+  methods: {
+    logout() {
+      this.$router.push({ path: "/login" });
+    },
+    cancel() {
+      this.$router.push({ path: "/" });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.content {
+  height: 90vh;
+}
+.seven24 {
+  font-size: 70px;
+}
+h2 {
+  color: #3ba2bd;
+}
+.confirm {
+  border: 1px solid #d1d0d0;
+  padding: 30px;
+  margin: 120px 300px 120px 300px;
+}
+#logout {
+  margin-right: 30px;
+}
+#cancel {
+  margin-left: 30px;
+}
+</style>

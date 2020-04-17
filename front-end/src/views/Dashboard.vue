983596dc9cd5998da56680b9088b6c29038fc48d
Tianze Wen <tianze.wen@ucdconnect.ie>
2020-04-17 19:35:46 +0800

    customer dashboard discussion

--- front-end/src/views/Dashboard.vue
+++ front-end/src/views/Dashboard.vue
@@ -44,6 +44,27 @@
                 <tr>
                   <td>{{$t("string.dashboard.dischargeDate")}}<span class="text-secondary">{{a.dischargeDate}}</span></td>
                 </tr>
+                
+                <a class="text-left" data-toggle="collapse" :href="'#d'+a.id" role="button" aria-expanded="false" :aria-controls="'d'+a.id" @click="getDiscussion(a.id)">
+                  <svg t="1587111515032" class="icon mb-1 mr-1" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2472" width="20" height="20"><path d="M808.533333 397.866667v-2.133334c0-157.866667-167.466667-286.933333-372.266666-286.933333S64 236.8 64 395.733333c0 88.533333 51.2 169.6 140.8 225.066667l24.533333 183.466667 152.533334-124.8c18.133333 2.133333 36.266667 3.2 54.4 3.2h2.133333c45.866667 72.533333 140.8 119.466667 246.4 119.466666h16l148.266667 112 11.733333-162.133333c62.933333-40.533333 98.133333-99.2 98.133333-163.2 1.066667-82.133333-57.6-154.666667-150.4-190.933333zM378.666667 635.733333l-9.6-1.066666-107.733334 88.533333-17.066666-129.066667-9.6-5.333333c-82.133333-46.933333-128-117.333333-128-193.066667 0-134.4 148.266667-244.266667 329.6-244.266666s329.6 109.866667 329.6 244.266666C765.866667 530.133333 617.6 640 436.266667 640c-19.2 0-38.4-2.133333-57.6-4.266667z m452.266666 85.333334l-9.6 5.333333-8.533333 105.6-98.133333-74.666667-7.466667 1.066667c-7.466667 0-14.933333 1.066667-21.333333 1.066667-81.066667 0-153.6-29.866667-196.266667-78.933334 160-18.133333 288-114.133333 314.666667-237.866666 69.333333 29.866667 113.066667 85.333333 113.066666 146.133333 0 51.2-32 99.2-86.4 132.266667z" p-id="2473" fill="#8a8a8a"></path><path d="M245.333333 317.866667h373.333334v42.666666H245.333333zM245.333333 434.133333h373.333334v42.666667H245.333333z" p-id="2474" fill="#8a8a8a"></path></svg>
+                  Discuss with attending doctor
+                </a>
+                <div class="collapse pt-1" :id="'d'+a.id">
+                  <div class="card card-body">
+                    <div class="message-container" :id="'dm'+a.id">
+                      <div v-for="(d,index) in discussions" :key="index">
+                        <div class="small text-secondary">{{d.postTime}}</div>
+                        <div class="bg-info rounded-lg py-1 px-2 my-4 text-left text-white bubble" :class="[d.employee?'mr-auto':'ml-auto']">{{d.content}}</div>
+                      </div>
+                    </div>
+                    <hr/>
+                    <div>
+                      <textarea class="form-control border-0" rows="2" placeholder="input your message here" v-model="messageText[a.id]"></textarea>
+                      <div class="text-right pt-1"><button class="btn btn-outline-info rounded" @click="postDiscussion(a.id)">Send</button></div>
+                    </div>
+                  </div>
+                </div>
+                
               </tbody>
             </table>
           </div>
@@ -195,6 +216,8 @@
         messageFailure: false,
         showButton: true,
         deleteId: -1,
+        messageText: {},
+        discussions: [],
       };
     },
 
@@ -210,7 +233,7 @@
     },
 
     created() {
-      document.title = `Dashboard | ${this.hospital}`;
+      document.title = this.$t("string.user.dashboard") + " | " + this.hospital;
     },
 
     mounted() {
@@ -346,6 +369,101 @@
             }
           });
       },
+
+      getDiscussion(discussionId) {
+        let _this = this;
+        this.discussion=[];
+
+        this.$axios({
+            method: 'get',
+            url: this.$global.request("discussion/"+discussionId),
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              "Authorization": "bearer " + this.$token.getToken(1)
+            }
+          })
+          .then(function (response) {
+            console.log(response);
+            if (response.data.code == 200) {
+              _this.discussions=response.data.data.discussions;
+            }
+            if (response.data.code == 400) {
+              $('.toast').toast('show');
+              _this.messageFailure=true;
+              _this.hintTitle=_this.$t("string.user.unknowError");
+              _this.hintText=response.data.msg+_this.$t("string.user.unknowErrorHint");
+            }
+          })
+          .catch(function (error) {
+            if(!error.response==undefined){
+            if (error.response.status == 401) {
+              _this.$token.removeToken();
+              _this.$router.push({
+                name: 'LogIn',
+                query: {
+                  message: _this.$t("string.appointment.loginExpired"),
+                  from: "/dashboard"
+                }
+              });
+            } }else {
+              $('.toast').toast('show');
+              console.log(error);
+              _this.messageFailure=true;
+              _this.hintTitle=_this.$t("string.user.unknowError");
+              _this.hintText=response.data.msg+_this.$t("string.user.unknowErrorHint");
+            }
+          });
+      },
+
+      postDiscussion(appointmentId) {
+        let _this = this;
+
+        this.$axios({
+            method: 'post',
+            url: this.$global.request("discussion"),
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              "Authorization": "bearer " + this.$token.getToken(1)
+            },
+            data: this.$qs.stringify({
+              appointmentId: appointmentId,
+              token: this.$token.getToken(0),
+              content: this.messageText[appointmentId]
+            })
+          })
+          .then(function (response) {
+            _this.messageText[appointmentId]="";
+            console.log(response);
+            if (response.data.code == 200) {
+              _this.getDiscussion(appointmentId);
+            }
+            if (response.data.code == 400) {
+              $('.toast').toast('show');
+              _this.messageFailure=true;
+              _this.hintTitle=_this.$t("string.user.unknowError");
+              _this.hintText=response.data.msg+_this.$t("string.user.unknowErrorHint");
+            }
+          })
+          .catch(function (error) {
+            if(!error.response==undefined){
+            if (error.response.status == 401) {
+              _this.$token.removeToken();
+              _this.$router.push({
+                name: 'LogIn',
+                query: {
+                  message: _this.$t("string.appointment.loginExpired"),
+                  from: "/dashboard"
+                }
+              });
+            } }else {
+              $('.toast').toast('show');
+              console.log(error);
+              _this.messageFailure=true;
+              _this.hintTitle=_this.$t("string.user.unknowError");
+              _this.hintText=response.data.msg+_this.$t("string.user.unknowErrorHint");
+            }
+          });
+      },
     }
 
   };
@@ -356,5 +474,15 @@
   .hide-sm{
     display: none;
   }
+}
+
+.bubble{
+  max-width: 70%; 
+  width: fit-content;
+}
+
+.message-container{
+  height: 300px; 
+  overflow-y: scroll;
 }
 </style>

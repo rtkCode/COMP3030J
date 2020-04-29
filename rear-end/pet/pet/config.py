b679056af115fb2e738733c653f1ca06822a73c7
Tianze Wen <tianze.wen@ucdconnect.ie>
2020-04-29 10:47:01 +0800

    optimize code format

--- rear-end/pet/pet/config.py
+++ rear-end/pet/pet/config.py
@@ -1,13 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET KEY') or 'XinTT SB'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir,'petHospital.db')
+        'sqlite:///' + os.path.join(basedir, 'petHospital.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
-    # CV_UPLOAD_DIR = os.path.join(basedir, 'uploaded_CV')
-    # IMG_UPLOAD_DIR = os.path.join(basedir, 'profile_images')
-
-
-# line 1-8 code from lecture 15 config.py, I change the secret_key
--- rear-end/pet/pet/key.py
+++ rear-end/pet/pet/key.py
@@ -1,1 +1,2 @@
+# This key is Internal Registration Code (IRC) for employees
 key = "Xin-TT107269362AAAJJJ"
